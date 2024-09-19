d0511f6cbc8bb656a44589fbb6d037e5d6de48d3
voytas <voytas@outlook.com>
2024-09-19 20:40:31 +0100

    Fix converter

--- src/ZX.Files/Extensions/TapFileExtensions.cs
+++ src/ZX.Files/Extensions/TapFileExtensions.cs
@@ -21,7 +21,7 @@ public static class TapFileExtensions
         {
             tzx.Blocks.Add(new StandardSpeedDataBlock
             {
-                Data = FileDataSerializer.Serialize(block).Skip(2).ToList(),
+                Data = FileDataSerializer.Serialize(block).ToList(),
                 PauseDuration = 1000,
             });
         }
